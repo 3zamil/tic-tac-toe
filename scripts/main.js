3884592d8494c88dfccf8406fa0436a56aab015e
3zamil <3zamil@gmail.com>
2019-12-04 23:06:28 +0300

    Final commit before deadline

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,24 @@
 let stateText;
 let pickedNo;
 let board = [];
-let numClicks = [];
-let rounds = []
-
-let ss = $("#player-0").val();
+let numClicks = 1;
+let round = 3;
 
 // Initialaization function:
 const init = function() {
   let dim1 = 3;
   let dim2 = 3;
 
-  
   let boxes = dim1 * dim2;
 
   let random = Math.floor(Math.random() * 2);
   pickedNo = random;
 
   $("#player-" + +!pickedNo).css("border-color", "red");
+  $("#player-" + pickedNo).css("border-color", "#ccc");
 
   board = [];
-  numClicks = [];
+  numClicks = 0;
   backMusic();
   // $("#myTune").prop("play", true)
   $("#player-0").prop("disabled", false);
@@ -50,27 +48,46 @@ const init = function() {
 };
 
 const results = function(winner) {
-  
-  $("input.numb").val($("input.numb").val() - 1)
-  if($("input.numb").val())
-    alert("ff")
-  // $("#player-" + winner).animate({ top: "250px" });
+  // when one player win
   $(".name-" + winner).animate({ bottom: "+=20%" }, "slow");
+  if (round == 0) {
+    ss = setTimeout(function() {
+      $(".name-0").animate({ bottom: "-29px" }, "slow");
+      $(".name-1").animate({ bottom: "-29px" }, "slow");
+    }, 3000);
+    round = 3;
+    // let myVar = setTimeout(alertFunc, 3000);
+  } else {
+    round--;
+  }
 };
+
 //ply X or O
 const play = function(event) {
-  $("input.numb").prop("disabled", true);
   let myTarget = event.target;
   pickedNo = +!pickedNo;
-  if (numClicks.length == 0) {
+  if (numClicks == 0) {
     $("#player-0").prop("disabled", true);
     $("#player-1").prop("disabled", true);
+    p1 = $("#player-0")
+      .val()
+      .substr(0, 1)
+      .toUpperCase();
+    p2 = $("#player-1")
+      .val()
+      .substr(0, 1)
+      .toUpperCase();
+    if (p1 == p2) {
+      p1 += pickedNo + 1;
+      p2 += +!pickedNo + 1;
+    }
+    stateText = [p1, p2];
   }
 
   $("#player-" + pickedNo).css("border-color", "#ccc");
   $("#player-" + +!pickedNo).css("border-color", "red");
 
-  numClicks.push(1);
+  numClicks++;
   let thePicked = stateText[pickedNo];
   $(myTarget).text(thePicked);
 
@@ -79,45 +96,33 @@ const play = function(event) {
   let boxId = $(myTarget).attr("id");
   board[boxId] = thePicked;
 
+  //check winner
   if (board[1] == board[2] && board[1] == board[3]) {
-    // setTimeout(() => {
-    //   alert(board[1] + " WIN");
-    //   init();
-    // }, 400);
     results(pickedNo);
     init();
-
-    //check winner
   } else if (board[4] == board[5] && board[4] == board[6]) {
     results(pickedNo);
-    // alert(board[4] + " WIN");
     init();
   } else if (board[7] == board[8] && board[7] == board[9]) {
     results(pickedNo);
-    // alert(board[7] + " WIN");
     init();
   } else if (board[1] == board[4] && board[1] == board[7]) {
     results(pickedNo);
-    // alert(board[1] + " WIN");
     init();
   } else if (board[2] == board[5] && board[2] == board[8]) {
     results(pickedNo);
-    // alert(board[2] + " WIN");
     init();
   } else if (board[3] == board[6] && board[3] == board[9]) {
     results(pickedNo);
-    // alert(board[3] + " WIN");
     init();
   } else if (board[1] == board[5] && board[1] == board[9]) {
     results(pickedNo);
-    // alert(board[1] + " WIN");
     init();
   } else if (board[3] == board[5] && board[3] == board[7]) {
     results(pickedNo);
-    // alert(board[3] + " WIN");
     init();
   } else {
-    if (numClicks.length == 9) {
+    if (numClicks == 9) {
       alert("NO ONE WIN");
       init();
     }
@@ -135,6 +140,11 @@ const backMusic = function() {
 
 $(function() {
   init();
-  $("#start").click(init);
+  $("#start").click(function() {
+    $(".name-0").animate({ bottom: "-29px" }, "slow");
+    $(".name-1").animate({ bottom: "-29px" }, "slow");
+    round = 3;
+    init();
+  });
   // $("#sound").click(backMusic);
 });
